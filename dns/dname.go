30174dcaf36f6dd4fa4f17c2401c04f7abb92fca
g0rbe <mail@danielgorbe.com>
2023-07-18 23:41:42 +0200

    add DNAME

---
+++ dns/dname.go
@@ -0,0 +1,86 @@
+package dns
+
+import (
+	"fmt"
+
+	"github.com/miekg/dns"
+)
+
+var TypeDNAME uint16 = 39
+
+// QueryDNAME returns the target string.
+//
+// The other records are ignored.
+func QueryDNAME(name string) (string, error) {
+
+	a, err := Query(name, TypeDNAME)
+	if err != nil {
+		return "", err
+	}
+
+	for i := range a {
+
+		switch v := a[i].(type) {
+		case *dns.DNAME:
+			return v.Target, nil
+		case *dns.CNAME:
+			// Ignore CNAME
+			continue
+		default:
+			return "", fmt.Errorf("unknown type: %T", v)
+		}
+	}
+
+	return "", nil
+}
+
+// QueryDNAMEServer returns the target string. USe server s to query.
+//
+// The other records are ignored.
+func QueryDNAMEServer(name string, s string) (string, error) {
+
+	a, err := QueryServer(name, TypeDNAME, s)
+	if err != nil {
+		return "", err
+	}
+
+	for i := range a {
+
+		switch v := a[i].(type) {
+		case *dns.DNAME:
+			return v.Target, nil
+		case *dns.CNAME:
+			// Ignore CNAME
+			continue
+		default:
+			return "", fmt.Errorf("unknown type: %T", v)
+		}
+	}
+
+	return "", nil
+}
+
+// QueryDNAMERetry query for DNAME record and retry for MaxRetries times if an error occured.
+func QueryDNAMERetry(name string) (string, error) {
+
+	var (
+		r   string
+		err error = ErrInvalidMaxRetries
+	)
+
+	for i := 0; i < MaxRetries; i++ {
+
+		r, err = QueryDNAMEServer(name, getServer(i))
+		if err == nil {
+			return r, nil
+		}
+	}
+
+	return "", err
+}
+
+// IsSetA checks whether an A type record set for name.
+// NXDOMAIN is not an error here, because it means "not found".
+func IsSetDNAME(name string) (bool, error) {
+	return IsSet(name, TypeDNAME)
+}
