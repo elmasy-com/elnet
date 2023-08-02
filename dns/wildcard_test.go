8ec47f78fe0e637ebbb42999d9aec4e49a4ac902
g0rbe <mail@danielgorbe.com>
2023-08-02 06:38:33 +0200

    add wildcard test

--- dns/wildcard_test.go
+++ dns/wildcard_test.go
@@ -12,4 +12,13 @@ func TestIsWildcard(t *testing.T) {
 	if r {
 		t.Fatalf("Fail: www.example.com is reported a wildcard domain\n")
 	}
+
+	r, err = IsWildcard("test.cyberdivine.ch", TypeA)
+	if err != nil {
+		t.Fatalf("Fail: failed to check if test.cyberdivine.ch is a wildcard: %s\n", err)
+	}
+
+	if !r {
+		t.Fatalf("Fail: test.cyberdivine.ch is not reported a wildcard domain\n")
+	}
 }
