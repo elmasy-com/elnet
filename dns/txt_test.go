97848d7870925d5c6ac81664d57711392e8a988b
g0rbe <mail@danielgorbe.com>
2023-08-08 20:10:42 +0200

    test truncated messages

--- dns/txt_test.go
+++ dns/txt_test.go
@@ -1,12 +1,45 @@
 package dns
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestQueryTXT(t *testing.T) {
 
-	r, err := QueryTXT("elmasy.com")
+	srvs, err := NewServersStr(3, 1*time.Second, "8.8.8.8", "8.8.8.8", "8.8.8.8")
+	if err != nil {
+		t.Fatalf("FAIL: Failed to create servers: %s\n", err)
+	}
+
+	r, err := srvs.QueryTXT("elmasy.com")
 	if err != nil {
 		t.Fatalf("FAIL: %s\n", err)
+	}
+
+	if len(r) == 0 {
+		t.Fatalf("FAIL: invalid result length: %d\n", len(r))
+	}
+
+	for i := range r {
+		t.Logf("elmasy.com TXT -> %s\n", r[i])
+	}
+}
+
+func TestQueryTXTTruncated(t *testing.T) {
+
+	srvs, err := NewServersStr(3, 1*time.Second, "8.8.8.8", "8.8.8.8", "8.8.8.8")
+	if err != nil {
+		t.Fatalf("FAIL: Failed to create servers: %s\n", err)
+	}
+
+	r, err := srvs.QueryTXT("dkim._domainkey.danielgorbe.com")
+	if err != nil {
+		t.Fatalf("FAIL: %s\n", err)
+	}
+
+	if len(r) == 0 {
+		t.Fatalf("FAIL: invalid result length: %d\n", len(r))
 	}
 
 	for i := range r {
@@ -21,6 +54,26 @@ func TestTryQueryTXT(t *testing.T) {
 		t.Fatalf("FAIL: %s\n", err)
 	}
 
+	if len(r) == 0 {
+		t.Fatalf("FAIL: invalid result length: %d\n", len(r))
+	}
+
+	for i := range r {
+		t.Logf("elmasy.com TXT -> %s\n", r[i])
+	}
+}
+
+func TestTryQueryTXTTruncated(t *testing.T) {
+
+	r, err := TryQueryTXT("dkim._domainkey.danielgorbe.com")
+	if err != nil {
+		t.Fatalf("FAIL: %s\n", err)
+	}
+
+	if len(r) == 0 {
+		t.Fatalf("FAIL: invalid result length: %d\n", len(r))
+	}
+
 	for i := range r {
 		t.Logf("elmasy.com TXT -> %s\n", r[i])
 	}
@@ -29,6 +82,18 @@ func TestTryQueryTXT(t *testing.T) {
 func TestIsSetTXT(t *testing.T) {
 
 	r, err := IsSetTXT("elmasy.com")
+	if err != nil {
+		t.Fatalf("FAIL: %s\n", err)
+	}
+
+	if !r {
+		t.Fatalf("FAIL: TXT is not set for elmasy.com\n")
+	}
+}
+
+func TestIsSetTXTTruncated(t *testing.T) {
+
+	r, err := IsSetTXT("dkim._domainkey.danielgorbe.com")
 	if err != nil {
 		t.Fatalf("FAIL: %s\n", err)
 	}
